3d29db03205fd4b70408a39076b2f95bd050e036
azrielgoldman <goldmanazriel@yahoo.com>
2021-02-24 11:38:57 -0500

    Completed SinglePet Component

--- src/components/SinglePet.js
+++ src/components/SinglePet.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 class SinglePet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      adopted: false
+    }
+    this.handleChange = this.handleChange.bind(this);
+  };
+
+  handleChange() {
+    this.setState((prevState) => ({adopted: !prevState.adopted}))
+  }
+
   render() {
-    return <div className="single-pet"></div>;
+    const { name, description, species } = this.props.pet
+    const { adopted } = this.state;
+    return <div className={`single-pet ${adopted ? 'adopted' : ''}`}>
+      <div>{name}</div>
+      <div>{description}</div>
+      <div>{species}</div>
+      {adopted ? 'Adopted!' : 'Available'}
+      <button onClick={this.handleChange}>Toggle Status</button>
+    </div>;
   }
 }
 
